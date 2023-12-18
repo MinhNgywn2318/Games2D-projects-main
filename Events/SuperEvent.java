e186f43e07123d526e90bbf9e5fffb98e618c1df
VuTraMy66 <153701476+VuTraMy66@users.noreply.github.com>
2023-12-18 09:06:32 +0700

    Update SuperEvent.java

--- Events/SuperEvent.java
+++ Events/SuperEvent.java
@@ -27,7 +27,7 @@ public class SuperEvent {
     public static String annouces[];
     public static boolean isDoll = true;
     public static boolean isMan = true;
-    public static boolean isNPC3 = true;
+    public static boolean isMonster = true;
     private int index = 0;
     public boolean isFirst = true;
     public static boolean teleport = false;
@@ -236,7 +236,7 @@ public class SuperEvent {
         isMan = true;
         isFirst = true;
         closeDoor = false;
-        isNPC3 = true;
+        isMonster = true;
     }
 
     public void closeDoor() {
