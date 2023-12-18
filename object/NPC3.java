caf7225f01a552a5bd9d99d63e87687c7cdca9fb
VuTraMy66 <153701476+VuTraMy66@users.noreply.github.com>
2023-12-18 09:21:43 +0700

    Update and rename monster.java to NPC3.java

--- object/NPC3.java
+++ object/NPC3.java
@@ -1,11 +1,11 @@
 package object;
 import main.GamePanel;
 import javax.imageio.ImageIO;
-public class monster extends SuperObject {
+public class NPC3 extends SuperObject {
     GamePanel gp;
 
-    public monster(){
-        name = "monster";
+    public NPC3(){
+        name = "NPC3";
         try {
             image = ImageIO.read(getClass().getResourceAsStream("/picture/object/NPC_3.png"));
         } catch (Exception e) {
