99a045d0bd9a645e65fea0a5425fec667a97a5d5
Anh Minh <nganhminh23@gmail.com>
2024-01-13 23:23:08 +0700

    Update project

--- Games2D12/TextSreen/quizz/setQuestions.java
+++
@@ -1,8 +0,0 @@
-package TextSreen.quizz;
-
-
-public class setQuestions {
-public String text = "as";
-public boolean answer;
-
-}
--- Games2D12/tiles/Tiles.java
+++
@@ -1,8 +0,0 @@
-package tiles;
-import  java.awt.image.BufferedImage;
-public class Tiles {
-    public BufferedImage image;
-    public boolean collision = false; 
-    
-
-}
